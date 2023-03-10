c2b294e24085302e858dc1cf353b5e60418c4455
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-03-10 15:39:46 +0000

    added info to contact page

--- reactportfolio/src/components/navigation/Contact.js
+++ reactportfolio/src/components/navigation/Contact.js
@@ -5,6 +5,13 @@ function Contact(){
         <div>
             <h1>Ways To Contact Me</h1>
             <p>...don't.</p>
+            <p>Only joking! Here are my details:</p>
+            <ul>
+                <li>My Github</li>
+                <li>My Resume/CV</li>
+                <li>My Contact Number</li>
+                <li>My Socials</li>
+            </ul>
         </div>
     )
 };
