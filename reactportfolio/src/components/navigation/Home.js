304223727f7949189a7c3d71d73b5ffe3a735202
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-03-10 15:43:25 +0000

    folder organisation

--- reactportfolio/src/components/navigation/Home.js
+++ reactportfolio/src/components/navigation/Home.js
@@ -6,7 +6,8 @@ function Home(){
            =<div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Rebecca Sellers</h1>
-    <p class="lead">I am a Front-End Web Developer.</p>
+    <p class="lead">I am a Front-End Web Developer and I have recently gone freelance. It's time to start polishing up my skills.</p>
+    
   </div>
 </div>
         </div>
